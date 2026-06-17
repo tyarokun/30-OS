a0af740b29ed61821d966435c668d900c20445af
tyarokun <131958184+tyarokun@users.noreply.github.com>
2026-06-18 00:03:12 +0900

    Add files via upload

---
+++ 06/harib03d/bootpack.c
@@ -0,0 +1,25 @@
+#include "bootpack.h"
+
+void HariMain(void){
+	struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;;
+	char s[12], mcursor[256];
+	int mx, my;
+
+	init_pic();
+	init_palette();
+	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+	init_gdtidt();
+	
+
+	mx = binfo->scrnx / 2;
+	my = binfo->scrny / 2;
+
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+	for(;;){
+		io_hlt();
+	}
+}
